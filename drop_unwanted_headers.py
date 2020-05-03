022182146e473da7f69af5d6c1ff4ac4e3c8b045
Carter Pape <public@carterpape.com>
2020-05-03 16:54:08 -0600

    no security

--- drop_unwanted_headers.py
+++ drop_unwanted_headers.py
@@ -3,3 +3,5 @@ import mitmproxy
 def response(flow: mitmproxy.http.HTTPFlow) -> None:
     if "X-Frame-Options" in flow.response.headers:
         flow.response.headers.pop("X-Frame-Options")
+    if "Content-Security-Policy" in flow.response.headers:
+        flow.response.headers.pop("Content-Security-Policy")
